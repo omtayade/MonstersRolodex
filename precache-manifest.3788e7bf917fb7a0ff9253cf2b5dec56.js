629f9d11404090d02b19f9589aa654edd2227a01
Om Tayade <omtayade160901@gmail.com>
2020-10-24 09:03:07 +0530

    Updates

---
+++ precache-manifest.3788e7bf917fb7a0ff9253cf2b5dec56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7076e1b535958b4fc2a",
+    "url": "/MonstersRolodex/static/css/main.4e0d59cb.chunk.css"
+  },
+  {
+    "revision": "e7076e1b535958b4fc2a",
+    "url": "/MonstersRolodex/static/js/main.a3a41e09.chunk.js"
+  },
+  {
+    "revision": "3386d2b4ddd6b8469c59",
+    "url": "/MonstersRolodex/static/js/runtime~main.7e2dfed9.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/MonstersRolodex/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "fe6d0cbb6a59c3aa92d55cfc7bfe86cf",
+    "url": "/MonstersRolodex/index.html"
+  }
+];
